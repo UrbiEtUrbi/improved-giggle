41f12d4c36756604a4f5e32a6b07563b2b39c147
Urban Strajnar <urban.strajnar@trickytribe.com>
2024-01-24 08:17:15 +0100

    added moon, timer and color change

---
+++ Assets/Scripts/Runtime/Helpers/MoonFollower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonFollower : MonoBehaviour
+{
+
+    float y;
+    private void Start()
+    {
+        y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+}
