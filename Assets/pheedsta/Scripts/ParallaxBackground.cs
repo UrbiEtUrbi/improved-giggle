8e6057ef1b793d5124244f93c91a9d278391c0b6
pheedsta <markgalea77@gmail.com>
2024-01-29 15:49:03 +1000

    Updated parallax scroller to allow transitions

--- Assets/pheedsta/Scripts/ParallaxBackground.cs
+++ Assets/pheedsta/Scripts/ParallaxBackground.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 //+++++++++++++++++++++++++++++//
 // CLASS: ParallaxBackground
@@ -13,60 +14,23 @@ public class ParallaxBackground : MonoBehaviour {
 
     [SerializeField] private ParallaxLayer parallaxLayerPrefab;
     [SerializeField] private ParallaxData[] parallaxData;
-
-    [SerializeField] SpriteRenderer Moon;
-
-    //::::::::::::::::::::::::::::://
-    // Local Fields
-    //::::::::::::::::::::::::::::://
-
-    private Transform cameraTransform;
-    private ParallaxLayer[] parallaxLayers;
-    private Vector3 previousCameraPosition;
     
     //::::::::::::::::::::::::::::://
     // Unity Callbacks
     //::::::::::::::::::::::::::::://
 
-    private void Awake() {
-        // get main camera transform
-        cameraTransform = Camera.main.transform;
-    }
-
     private void Start() {
         // if a ParallaxLayer prefab is not set; we are done
         if (!parallaxLayerPrefab) return;
 
-        // initialise array
-        parallaxLayers = new ParallaxLayer[parallaxData.Length];
-
-        for (int i = 0; i < parallaxData.Length; i++) {
-            // instantiate new parallax layer and configure
-            ParallaxLayer parallaxLayer = Instantiate(parallaxLayerPrefab, transform);
-            parallaxLayer.Configure(parallaxData[i].backgroundSprite, i);
+        foreach (var data in parallaxData) {
+            // if the data layer is not enabled; just ignore it
+            if (!data.enabled) continue;
             
-            // add new layer to array
-            parallaxLayers[i] = parallaxLayer;
+            // instantiate a new ParallaxLayer for each ParallaxData and configure it
+            ParallaxLayer parallaxLayer = Instantiate(parallaxLayerPrefab, transform);
+            parallaxLayer.Configure(data);
         }
-        
-        // get the start position of the camera
-        previousCameraPosition = cameraTransform.position;
-    }
-
-    private void Update() {
-        // check that data and layers have the same count (this should never fail)
-        if (parallaxData.Length != parallaxLayers.Length) return;
-        
-        // get the distance moved by the camera and update previous position
-        float cameraX = cameraTransform.position.x - previousCameraPosition.x;
-
-        // move each layer by the horizontal speed provided
-        for (int i = 0; i < parallaxData.Length; i++) parallaxLayers[i].Move(cameraX, parallaxData[i].horizontalSpeed);
-
-        
-
-        // update position field (used for calcs next frame)
-        previousCameraPosition = cameraTransform.position;
     }
 }
 
@@ -76,6 +40,18 @@ public class ParallaxBackground : MonoBehaviour {
 
 [Serializable]
 public class ParallaxData {
-    public Sprite backgroundSprite;
-    public float horizontalSpeed = 1f;
+    public bool enabled = true;
+    public float moveSpeed = 1f;
+    public int sortingOrder;
+    public ParallaxSpriteData[] spriteData;
+}
+
+//+++++++++++++++++++++++++++++//
+// CLASS: ParallaxSpriteData
+//+++++++++++++++++++++++++++++//
+
+[Serializable]
+public class ParallaxSpriteData {
+    public Sprite sprite;
+    public int count = 1;
 }
--- Assets/pheedsta/Scripts/ParallaxLayer.cs
+++ Assets/pheedsta/Scripts/ParallaxLayer.cs
@@ -1,12 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxLayer : MonoBehaviour {
     
     //::::::::::::::::::::::::::::://
+    // Properties
+    //::::::::::::::::::::::::::::://
+
+    private float MinCameraX => GetMinCameraX();
+    private float MaxCameraX => GetMaxCameraX();
+    
+    //::::::::::::::::::::::::::::://
+    // Constants
+    //::::::::::::::::::::::::::::://
+
+    private const float screenMargin = 10f;
+    
+    //::::::::::::::::::::::::::::://
     // Local Fields
     //::::::::::::::::::::::::::::://
+
+    private int midIndex = 1;
+    private float minSpriteRendererX, maxSpriteRendererX, moveSpeed;
+    private Vector3 previousCameraPosition;
     
-    private float spriteWidth, xTotal;
+    private Camera mainCamera;
+    private Sprite[] sprites;
     private SpriteRenderer[] spriteRenderers;
     
     //::::::::::::::::::::::::::::://
@@ -14,55 +33,203 @@ public class ParallaxLayer : MonoBehaviour {
     //::::::::::::::::::::::::::::://
 
     private void Awake() {
-        // get sprite renderer components (should be three)
+        // get sprite renderer components (MUST be three)
         spriteRenderers = transform.GetComponentsInChildren<SpriteRenderer>();
+        Debug.Assert(spriteRenderers.Length == 3, "There must be exactly three SpriteRenderer components attached to ParallaxLayer.");
+        
+        // get the main camera
+        mainCamera = Camera.main;
+        Debug.Assert(mainCamera, "The main camera could not be found.");
+    }
+
+    private void Start() {
+        // get the starting position of the camera
+        previousCameraPosition = mainCamera.transform.position;
+    }
+
+    private void Update() {
+        MoveSpriteRenderers();
     }
     
     //-----------------------------//
     // Configuration
     //-----------------------------//
     
-    public void Configure(Sprite backgroundSprite, int index) {
-        // get the width of the sprite (used in calcs)
-        spriteWidth = backgroundSprite.bounds.size.x;
+    public void Configure(ParallaxData parallaxData) {
+        // set move speed (used when calculating how far to move it)
+        moveSpeed = parallaxData.moveSpeed;
         
-        // calculate the starting position of the first sprite
-        float x = (spriteRenderers.Length - 1f) / 2f * -spriteWidth;
+        // initialise an empty List to hold all Sprites in order (we will convert this to an array when done)
+        List<Sprite> spriteList = new List<Sprite>();
+        
+        // add sprites to list
+        foreach (var spriteData in parallaxData.spriteData) {
+            for (int i = 0; i < spriteData.count; i++) spriteList.Add(spriteData.sprite);
+        }
+        
+        // convert list to array
+        sprites = spriteList.ToArray();
+        
+        // if we don't have at least three Sprites (one for each SpriteRenderer) we are done
+        if (sprites.Length < spriteRenderers.Length) return;
+        
+        // initialise fields used in for loop
+        float totalWidth = 0f;
+        
+        for (int i = 0; i < spriteRenderers.Length; i++) {
+            // get the SpriteRenderer and Sprite (we know index will be in range for both arrays)
+            SpriteRenderer spriteRenderer = spriteRenderers[i];
+            Sprite sprite = sprites[i];
+            
+            // update SpriteRenderer with Sprite and sorting order
+            spriteRenderer.sprite = sprite;
+            spriteRenderer.sortingOrder = parallaxData.sortingOrder;
 
-        foreach (var spriteRenderer in spriteRenderers) {
-            // update background sprite and move into position
-            spriteRenderer.sprite = backgroundSprite;
-            spriteRenderer.transform.position = new Vector3(x, 0f, 0f);
+            // increment total width (we will use this to determine starting positions of SpriteRenderers)
+            totalWidth += sprite.bounds.size.x;
+        }
+        
+        // get the camera position
+        Vector3 cameraPosition = mainCamera.transform.position;
+        
+        // calculate the extents of the SpriteRenderers
+        minSpriteRendererX = cameraPosition.x + totalWidth / -2f;
+        maxSpriteRendererX = minSpriteRendererX + totalWidth;
+
+        // get the starting point (this value will increment)
+        float x = minSpriteRendererX;
+        
+        foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+            // get the sprite width
+            float spriteWidth = spriteRenderer.sprite.bounds.size.x;
             
-            // update the order so it renders correctly
-            spriteRenderer.sortingOrder = index;
+            // update the position of the SpriteRenderer (midpoint)
+            spriteRenderer.transform.position = new Vector3(x + spriteWidth / 2f, cameraPosition.y, 0f);
             
-            // increment x for next sprite
+            // increment x for start of next sprite
             x += spriteWidth;
         }
     }
     
-    //-----------------------------//
-    // Movement
-    //-----------------------------//
+    //::::::::::::::::::::::::::::://
+    // Property Getters
+    //::::::::::::::::::::::::::::://
 
-    public void Move(float x, float xSpeed) {
-        // calculate how far to move background (does not include camera movement)
-        float moveX = -x * xSpeed;
+    private float GetMinCameraX() {
+        return mainCamera.ViewportToWorldPoint(Vector3.zero).x;
+    }
+
+    private float GetMaxCameraX() {
+        return mainCamera.ViewportToWorldPoint(Vector3.one).x;
+    }
+    
+    //::::::::::::::::::::::::::::://
+    // Moving SpriteRenderers
+    //::::::::::::::::::::::::::::://
+    
+    private void MoveSpriteRenderers() {
+        // get the new position of the camera
+        Vector3 currentCameraPosition = mainCamera.transform.position;
         
-        // increment total
-        xTotal += moveX;
+        // get the distances moved by the camera
+        float cameraMoveX = currentCameraPosition.x - previousCameraPosition.x;
+        float cameraMoveY = currentCameraPosition.y - previousCameraPosition.y;
         
-        // reset movement to provide infinite scrolling
-        if (spriteWidth < xTotal) {
-            xTotal -= spriteWidth;
-            moveX -= spriteWidth;
-        } else if (xTotal < -spriteWidth) {
-            xTotal += spriteWidth;
-            moveX += spriteWidth;
+        // update value for the next time
+        previousCameraPosition = currentCameraPosition;
+
+        // calculate how far to move background (include camera movement)
+        float moveX = -cameraMoveX * moveSpeed + cameraMoveX;
+        float moveY = -cameraMoveY * moveSpeed + cameraMoveY;
+        
+        foreach (var spriteRenderer in spriteRenderers) {
+            // get the current position of the SpriteRenderer
+            Vector3 position = spriteRenderer.transform.position;
+            
+            // update X and Y values
+            position.x += moveX;
+            position.y += moveY;
+            
+            // reset to new position
+            // ReSharper disable once Unity.InefficientPropertyAccess
+            spriteRenderer.transform.position = position;
         }
         
-        // move sprites (include camera movement)
-        foreach (var spriteRenderer in spriteRenderers) spriteRenderer.transform.position += Vector3.right * (x + moveX);
+        // update SpriteRenderer extents
+        minSpriteRendererX += moveX;
+        maxSpriteRendererX += moveX;
+        
+        // shuffle renderers so it is an infinite scroll
+        if (MinCameraX < minSpriteRendererX) {
+          ShuffleSpriteRenderersLeft();
+        } else if (maxSpriteRendererX < MaxCameraX) {
+            ShuffleSpriteRenderersRight();
+        }
+    }
+
+    private void ShuffleSpriteRenderersRight() {
+        // if the new mid index is going to be out of range; we are done 
+        if (midIndex + 1 >= sprites.Length - 1) return;
+        
+        // increment mid index
+        midIndex++;
+        
+        // get the FIRST SpriteRenderer (this will be reused and moved to the END of the array) and the NEXT sprite (we know this index will be in range)
+        SpriteRenderer reuseSpriteRenderer = spriteRenderers[0];
+        Sprite sprite = sprites[midIndex + 1];
+        
+        // update SpriteRenderer with new sprite
+        reuseSpriteRenderer.sprite = sprite;
+        
+        // initialise fields for cals
+        Transform spriteRendererTransform = reuseSpriteRenderer.transform;
+        Vector3 position = spriteRendererTransform.position;
+        float spriteWidth = sprite.bounds.size.x;
+        
+        // calculate new position for SpriteRenderer and update
+        position.x = maxSpriteRendererX + spriteWidth / 2f;
+        spriteRendererTransform.position = position;
+        
+        // increment left and right extents
+        minSpriteRendererX += spriteWidth;
+        maxSpriteRendererX += spriteWidth;
+        
+        // reshuffle the SpriteRenderers (put reused one to the end)
+        spriteRenderers[0] = spriteRenderers[1];
+        spriteRenderers[1] = spriteRenderers[2];
+        spriteRenderers[2] = reuseSpriteRenderer;
+    }
+    
+    private void ShuffleSpriteRenderersLeft() {
+        // if the new mid index is going to be out of range; we are done 
+        if (midIndex - 1 < 1) return;
+        
+        // decrement mid index
+        midIndex--;
+        
+        // get the LAST SpriteRenderer (this will be reused and moved to the START of the array) and the PREVIOUS sprite (we know this index will be in range)
+        SpriteRenderer reuseSpriteRenderer = spriteRenderers[2];
+        Sprite sprite = sprites[midIndex - 1];
+        
+        // update SpriteRenderer with new sprite
+        reuseSpriteRenderer.sprite = sprite;
+        
+        // initialise fields for cals
+        Transform spriteRendererTransform = reuseSpriteRenderer.transform;
+        Vector3 position = spriteRendererTransform.position;
+        float spriteWidth = sprite.bounds.size.x;
+        
+        // calculate new position for SpriteRenderer and update
+        position.x = minSpriteRendererX - spriteWidth / 2f;
+        spriteRendererTransform.position = position;
+        
+        // decrement left and right extents
+        minSpriteRendererX -= spriteWidth;
+        maxSpriteRendererX -= spriteWidth;
+        
+        // reshuffle the SpriteRenderers (put reused one to the start)
+        spriteRenderers[1] = spriteRenderers[0];
+        spriteRenderers[2] = spriteRenderers[1];
+        spriteRenderers[0] = reuseSpriteRenderer;
     }
 }
