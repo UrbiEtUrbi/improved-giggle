e0da9f0646a69c01d9745903ea81c2399391613e
Urban Strajnar <urban.strajnar@trickytribe.com>
2024-01-31 22:43:21 +0100

    trigger on death

--- Assets/Scripts/Runtime/DialogSequenceEnemy.cs
+++ Assets/Scripts/Runtime/DialogSequenceEnemy.cs
@@ -13,12 +13,31 @@ public class DialogSequenceEnemy : MonoBehaviour
     [SerializeField]
     float Offset;
 
+    [SerializeField]
+    bool TriggerOnDeath;
+
     private void Update()
     {
+
+        if (TriggerOnDeath)
+        {
+            return;
+        }
         if (Vector3.Distance(ControllerGame.Player.transform.position, transform.position) < Distance)
         {
             ControllerGame.ControllerDialog.TriggerDialogue(ID, transform, new Vector3(0, Offset, 0));
             Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (gameObject.scene.isLoaded)
+        {
+            if (TriggerOnDeath)
+            {
+                ControllerGame.ControllerDialog.TriggerDialogue(ID, ControllerGame.Player.transform, new Vector3(0, 2, 0));
+            }
         }
     }
 
